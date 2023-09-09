44da69502e8bb82791a7beb23afb2d6812c39396
Marcos Moraes <contato@marcosamoraes.com>
2023-09-08 23:42:29 -0300

    contract

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -107,20 +107,19 @@
                                                         <i class="fas fa-file"></i>
                                                     </x-button>
                                                 </a>
+                                            @else
+                                                <a href="https://wa.me/{{ preg_replace('/\D/', '', $order->company->phone) }}?text=Link do contrato do Achei16: {{ route('orders.contract', $order->uuid) }}" target="_blank">
+                                                    <x-button variant="whatsapp" title="Link do contrato">
+                                                        <i class="fab fa-whatsapp"></i>
+                                                    </x-button>
+                                                </a>
+                                                <a href="{{ route('orders.contract', $order->uuid) }}" target="_blank">
+                                                    <x-button variant="black" title="Link do contrato">
+                                                        <i class="fas fa-file"></i>
+                                                    </x-button>
+                                                </a>
                                             @endif
                                             @if ($order->status === 'pending')
-                                                @if (!$order->contract_url)
-                                                    <a href="https://wa.me/{{ preg_replace('/\D/', '', $order->company->phone) }}?text=Link do contrato do Achei16: {{ route('orders.contract', $order->uuid) }}" target="_blank">
-                                                        <x-button variant="whatsapp" title="Link do contrato">
-                                                            <i class="fab fa-whatsapp"></i>
-                                                        </x-button>
-                                                    </a>
-                                                    <a href="{{ route('orders.contract', $order->uuid) }}" target="_blank">
-                                                        <x-button variant="black" title="Link do contrato">
-                                                            <i class="fas fa-file"></i>
-                                                        </x-button>
-                                                    </a>
-                                                @endif
                                                 <a href="{{ route('orders.payment.generate', $order->id) }}" target="_blank">
                                                     <x-button variant="info" title="gerar link para pagamento">
                                                         <i class="fas fa-money-bill"></i>
