009b38c4618d3366fc41ceb47f65c2d306a94086
Marcos Moraes <contato@marcosamoraes.com>
2023-09-14 17:42:00 -0300

    task scheduler test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,6 +16,9 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->command('app:send-email-notice-plan-will-expire')->dailyAt('10:00');
         $schedule->command('app:send-email-update-company-data')->dailyAt('10:00');
+        $schedule->call(function () {
+            Log::info('Teste de agendamento de tarefa');
+        })->everyMinute();
     }
 
     /**
